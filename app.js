5fd6ccb1702c21feeaef0bb802ab48ba1b303922
Paul Hwang <mothcar@gmail.com>
2017-05-30 18:06:36 +0900

    app.js 정리

--- app.js
+++ app.js
@@ -3,24 +3,12 @@ var app = express();
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
+var mongoDB = 'mongodb://root:1748asdf@ds157571.mlab.com:57571/meeteamdb';
+var db
+
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}))
-
-app.get('/', (req, res) => {
-  db.collection('test').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    // renders index.ejs
-    res.render('index.ejs', {quotes: result})
-  })
-})
-
-console.log("root name : " + __dirname)
-
-//Set up default mongoose connection
-var mongoDB = 'mongodb://root:1748asdf@ds157571.mlab.com:57571/meeteamdb';
-
-var db
 
 MongoClient.connect(mongoDB, (err, database) => {
 	if (err) return console.log(err)
@@ -28,6 +16,14 @@ MongoClient.connect(mongoDB, (err, database) => {
 	app.listen(3000, () => {
 		console.log('listening on 3000')
 	})
+})
+
+app.get('/', (req, res) => {
+  db.collection('test').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
 })
 
 app.post('/quotes', (req, res) => {
