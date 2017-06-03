7bec43bf556575250a70de672a370cadd6b9d7e5
KimCheolhwan <jasd0330@naver.com>
2017-06-03 19:48:05 +0900

    1

---
+++ app_file.js
@@ -0,0 +1,72 @@
+// get mongoose package
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/DB');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  	console.log("open");
+});
+
+// creates DB schema
+var userSchema = mongoose.Schema({
+    name: 'string',
+    title: 'string',
+    pig:'string',
+    move:'string',
+    comment:['string']
+
+});
+
+// compiels our schema into a model
+var MeTeam = mongoose.model('meteam', userSchema);
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.locals.pretty = true;
+app.get('/', (req, res) => {
+  MeTeam.find({},(err, result) => {
+    if (err) return console.log(err) // renders index.ejs
+    res.render('index.ejs', {topics: result});
+  })
+})
+app.post('/remove',(req,res)=>{
+    MeTeam.update(
+    { pig: req.body.pig},
+    { $pull: { comment:req.body.remove}},
+    (err, result) => {
+      if (err) {
+        res.status(500)
+      }
+    }
+  );
+  res.redirect('/');
+});
+
+app.post('/',(req,res)=>{
+  MeTeam.update(
+  { pig: req.body.pig },
+  { $push:  { comment: req.body.desc }},
+  {safe: true, upsert: true, new : true},
+  (err, result) => {
+    if (err) {
+      res.status(500)
+    }
+ }
+);
+  MeTeam.find({},(err, result) => {
+    if (err) return console.log(err) // renders index.ejs
+
+    res.render('index.ejs', {topics: result});
+    console.log(result);
+  })
+});
+
+
+app.listen(3000,function(){
+  console.log('Connected, 3000 Port!');
+});
